3f8b84eb0e58b9eede8789e5d83699e93fafa443
manu <manu@manu-Aspire-E3-112>
2016-06-01 22:20:41 +0200

    ajout d'une boucle

--- testGit/src/testGit/EssaiFr.java
+++ testGit/src/testGit/EssaiFr.java
@@ -3,5 +3,8 @@ package testGit;
 public class EssaiFr {
 	public static void main(String[] args) {
 		System.out.println("coucou le monde");
+		for (int i = 0; i < 5; i++) {
+			System.out.println("oui bonjour");
+		}
 	}
 }
